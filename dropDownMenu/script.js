5acb8d6fb714e780e3207416a9d7f72cc7b65662
Arbati6 <k33.raf@gmail.com>
2022-05-28 16:51:53 +0200

    added media querys

--- dropDownMenu/script.js
+++ dropDownMenu/script.js
@@ -9,3 +9,31 @@ searchBox.addEventListener('click', () => {
         searchBox.classList.replace('bx-x', 'bx-search');
     }
 })
+
+let menuOpenBtn = document.querySelector('.navbar .bx-menu');
+let menuCloseBtn = document.querySelector('.nav-links .bx-x');
+let navLinks = document.querySelector('.nav-links');
+
+
+// menuOpenBtn.addEventListener('click', ()=> {
+//     navLinks.style.left="0";
+// })
+
+// menuCloseBtn.addEventListener('click', ()=> {
+//     navLinks.style.left="-100%";
+// })
+
+let htmlcssArrow = document.querySelector('.htmlcss-arrow') 
+htmlcssArrow.addEventListener("click", ()=> {
+    navLinks.classList.toggle("show1");
+})
+
+let moreArrow = document.querySelector('.more-arrow') 
+moreArrow.addEventListener("click", ()=> {
+    navLinks.classList.toggle("show2");
+})
+
+let jsArrow = document.querySelector('.js-arrow') 
+jsArrow.addEventListener("click", ()=> {
+    navLinks.classList.toggle("show3");
+})
